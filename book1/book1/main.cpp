8af7401323f093d8673bb9320bfff0422a525579
Игорь Игорь <zhuglya-igor@mail.ru>
2019-10-02 22:30:39 +0300

    update_02.10

---
+++ book1/book1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string inventory[10];
+	inventory[0] = "dude";
+	inventory[1] = "lol";
+	inventory[2] = "fellow";
+
+	cout << inventory[1].size() << endl;
+
+	return 0;
+
+}
