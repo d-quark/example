a8b42a7f4e379ae08abc3dc37281e3c7c13f640f
Игорь Игорь <zhuglya-igor@mail.ru>
2020-01-28 18:01:11 +0300

    1.28. work

--- book1/book1/chapter4_ex1.cpp
+++ book1/book1/chapter4_ex1.cpp
@@ -1,10 +1,44 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
+using namespace std;
 
 int main() 
 {
 
+	vector<string> games;
+	games.push_back("diablo");
+	vector<string>::iterator myIterator;
+	vector<string>::const_iterator iter;
 
+	cout << "Now you have " << games.capacity() << " games" << endl;
+	cout << "What do you want?\n";
+	cout << "1) Add your favorite games.\n2)Delete your games.\n3)Show all your games\n";
+	int choice;
+	
+	cin >> choice;
 
+	if (choice == 1) {
+		cout << "Please type a game's name you want to add to the list: ";
+		string game;
+		cin >> game;
+		games.push_back(game);
+	}
+
+	if (choice == 2) {
+		cout << "What game do you want to remove? ";
+		string game;
+		cin >> game;
+		if ( find(games.begin(), games.end(), game ) != games.end()) {
+			
+		}
+	}
+
+	cout << "Now you have " << games.capacity() << " games" << endl;
+	for (iter = games.begin(); iter != games.end(); ++iter) {
+		cout << *iter << endl;
+	}
+
+	return 0;
 }
